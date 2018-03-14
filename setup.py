c4b0c406596fff86cc84439b19269f674b46edee
Victor Ng <victor@crankycoder.com>
2018-03-14 15:12:15 -0400

    added setup.py for tox and docs for sphinx documentation

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='dockerflow-flask',
+    version='0.1dev',
+    author='Mozilla Foundation',
+    author_email='vng@mozilla.com',
+    url='https://github.com/crankycoder/dockerflow-flask-template',
+)
